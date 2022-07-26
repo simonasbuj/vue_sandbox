e5b1a0caf5bb59953d4fb4e9938724f7c7addd51
unknown <simonas.b@hotmail.com>
2022-07-26 14:55:05 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+	//template: '<h2> my temp </h2>'
+	data(){
+		return {
+			my_var: 'The Final Empire',
+			another_var: 'BOI',
+			my_number: 68
+		}
+	}
+})
+
+app.mount('#app')
