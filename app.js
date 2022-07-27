3b73f88c5b17f87542dd89f073773f31b505955d
unknown <simonas.b@hotmail.com>
2022-07-27 11:47:33 +0300

    v-for added, tailwind centered blocks too

--- app.js
+++ app.js
@@ -5,13 +5,34 @@ const app = Vue.createApp({
 			my_var: 'The Final Empire',
 			another_var: 'BOI',
 			my_number: 68,
-			shouldShow: true
+			shouldShow: true,
+			shouldShowText: "Hide",
+			positionLeft: 100,
+			psoitionTop: 20,
+			movies: [
+				{title: "The Sea Beast", length: "1 hour 43 mins"},
+				{title: "Prehistoric Episode 4", length: "45 mins"},
+				{title: "Mandalorian Season 1", length: "6 hours 17 mins"}
+			]
 		}
 	},
 	methods: {
-		changeTitle(newTitle){
+		changeTitle(newTitle) {
 			this.my_var = newTitle
+		},
+		showOrHide() {
+			this.shouldShow = !this.shouldShow
+			this.shouldShow == true ? this.shouldShowText = "Hide" : this.shouldShowText = "Show"
+		},
+		handleMousemove(e) {
+			console.log("mouse coordinates in body: " + e.offsetX + " " + e.offsetY + " " + this.positionLeft);
+			this.positionLeft = e.clientX
+			this.psoitionTop = e.clientY
+			console.log(e)
 		}
+	},
+	mounted() {
+		//document.addEventListener('mousemove', this.handleMousemove)
 	}
 })
 
